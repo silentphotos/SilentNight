c6f9b61b732b5206e11315655383a920ebe2b918
Renden Yoder <rendenyoder@Rendens-iMac.local>
2017-10-24 23:29:26 -0700

    Added button animation.

--- app/src/main/java/com/afterrabble/silentnight2/MainActivity.java
+++ app/src/main/java/com/afterrabble/silentnight2/MainActivity.java
@@ -2,12 +2,35 @@ package com.afterrabble.silentnight2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SoundEffectConstants;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mCameraButton;
+    long lastDown;
+    long lastDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraButton = (Button) findViewById(R.id.imageButton);
+        mCameraButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown = System.currentTimeMillis();
+                    view.playSoundEffect(SoundEffectConstants.CLICK);
+                    mCameraButton.setBackgroundResource(R.drawable.camera_button_image_pressed);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    lastDuration = System.currentTimeMillis() - lastDown;
+                    mCameraButton.setBackgroundResource(R.drawable.camera_button_image);
+                }
+                return true;
+            }
+        });
     }
 }
